016fdbd042652928103a64755edbda19a546f22d
kalpaj12 <kalpaj.agrawalla02@gmail.com>
2019-11-18 16:51:24 +0530

    [chore]: Fixes in sort param

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def job():
     current_network_out = current_network.out
 
     if((current_network_out.find("I-ON") != -1) or (current_network_out.find("ION") != -1)):
-        ion_ap = 'nmcli -f ssid,bssid,signal,bars,freq,rate,in-use  dev wifi | grep -E \'ION|I-ON\' | sort -k5 -k3 -s -r | head -n 1 | awk \'{print $1} {print $2} {print $9} \''
+        ion_ap = 'nmcli -f ssid,bssid,signal,bars,freq,rate,in-use  dev wifi | grep -E \'ION|I-ON\' | sort -k5 -n -r -s | head -n 1 | awk \'{print $1} {print $2} {print $9} \''
         output = delg.run(ion_ap)
 
         optimal_ssid = output.out.splitlines()[0]
